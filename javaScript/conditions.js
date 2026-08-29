87f8634e3e1f2112601f02ceb7a09c792b786352
Vivek Nagare <nagarevivek206@gmail.com>
2026-08-29 13:48:55 +0000

    add conditions and switch examples; implement age-based ticket pricing and voting eligibility checks

---
+++ javaScript/conditions.js
@@ -0,0 +1,25 @@
+// let age = 18;
+
+
+
+// if(age>=18) {
+//     console.log("your are eligible for vote ");
+    
+// }
+// else{
+//     console.log("your are not eligible for vote  ");
+// }
+    
+
+let age = 60;
+
+if(age < 12){
+    console.log("Child ticket ");
+}
+else if(age >= 12 && age <60){
+    console.log("Adult ticket");
+}
+else{
+    console.log("senior citizen");  
+
+}
---
+++ javaScript/switch.js
@@ -0,0 +1,97 @@
+// let userRole =  "abc";
+
+// if(userRole == "admin"){
+//     console.log("full access granted");
+    
+// }
+// else if(userRole == "editor"){
+//     console.log("Welcome to editor world");
+    
+// }
+// else {
+//     console.log("Welcome to employee");
+    
+// }
+
+
+
+// let userRole = "editor";
+
+// switch(userRole){
+//     case "admin":
+//         console.log("full access granted");
+//         break;
+//     case "editor":
+//         console.log("Welcome to editor world");
+//         break;
+//     case "student":
+//         console.log("Welcome to student world");
+//         break;
+//     default:
+//         console.log("Welcome to candidate world");
+// }
+
+
+
+// let weeks = "sunday";
+
+// switch(weeks){
+//     case "monday":
+//         console.log("it is monday");
+//         break;
+//     case "tuesday":
+//         console.log("it is mid week");
+//         break;
+//     case "wednesday":
+//         console.log("it is mid week");
+//         break;
+//     case "thursday":
+//         console.log("it is mid week");
+//         break;
+//     case "friday":
+//         console.log("ohh Happy Friday..!");
+//         break;
+//     case "saturday":
+//         console.log("Happ weekend...!");
+//         break;
+//     case "sunday":
+//         console.log("Happ weekend..!");
+       
+        
+// }
+
+
+// let day =1;
+
+// switch(day){
+//     case 0:
+//     case 6:
+//         console.log("Happy weekend...");
+//         break;
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//         console.log("it is mid weeks");
+//         break;
+//     case 5:
+//         console.log("Happ Friday");   
+// }
+
+
+
+// let age = 20;
+// let msg ;
+
+// if(age >= 18){
+//     msg = "can work";
+// }
+// else{
+//     msg = "cannot work";
+// }
+// console.log(msg);
+
+let age = 57;
+let msg;
+msg = age >= 18 ? "can vote..!" : "cannot vot..!"
+console.log(msg);
