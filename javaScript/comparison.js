df1fc14eac98bf030650b658234a7032caaed50b
Vivek Nagare <nagarevivek206@gmail.com>
2026-08-29 07:38:51 +0000

    add comparison examples for data types and equality checks

---
+++ javaScript/comparison.js
@@ -0,0 +1,27 @@
+let age = 16;
+console.log(age == 16);
+
+let firstName = "vicky";
+let lastName = "vicky";
+console.log(firstName == lastName);
+
+let a = 1;
+let b =(Number(true)==1);
+console.log(b);
+
+
+console.log(null === false);
+
+console.log(null == false);
+
+console.log(undefined == 0);
+
+console.log('true' == true);
+console.log('false' == false);
+
+console.log('01' == 1);
+console.log('010' == 10);
+
+console.log(0 == null);
+console.log(0 == undefined);
+
