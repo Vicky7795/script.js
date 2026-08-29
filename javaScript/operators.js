e3a0e3ebee14d201512525d65552b8f207964875
Vivek Nagare <nagarevivek206@gmail.com>
2026-08-29 09:08:00 +0000

    add operators.js and implement logical operator examples; add prompt-sync and strip-ansi modules with documentation

---
+++ javaScript/operators.js
@@ -0,0 +1,34 @@
+// console.log(true && false);
+
+// console.log(123 && 0);
+
+// console.log('hello' && 'hi');
+
+
+// && operators in js how it will be working
+// if one values is checked false then value will false
+
+// console.log(0 && hello);
+// console.log(' ' && 'hello');
+// console.log('hi' && 'hello');
+
+// || operators in js how it will be working
+// the value always must be return true
+
+// console.log(true || 'hello');
+// console.log('hi' || 'hello');
+// console.log('' || 'default');
+
+
+// // ! not operator how it will be working in js
+//  console.log(!'');
+//  console.log(!123);
+//  console.log(![]);
+//  console.log(!true);
+//  console.log(!false);
+ 
+
+let count = 0;
+let result = count || 10;
+console.log(result);
+
